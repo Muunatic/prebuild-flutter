4b22ef64f51ebb622904059186a5fd3fe54fa598
Mephysics <ichigoez0@gmail.com>
2021-10-20 11:19:21 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/mephysics/mephystos/pushdb/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mephysics.mephystos.pushdb
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Hello, World!'),),
+        body: Center(child: Text('Hello, World!')),
+      ),
+    );
+  }
+}
