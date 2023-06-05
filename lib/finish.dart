58b05ef572ca5e7001b9564fe8953b4f68f2e163
Muunatic <ichigoez0@gmail.com>
2023-06-05 20:30:41 +0700

    feat: add finish order UI

--- lib/finish.dart
+++ lib/finish.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'topup.dart';
+import 'order.dart';
+import 'homepage.dart';
 
 void main() {
     runApp(const MainApp());
@@ -15,11 +18,34 @@ class MainApp extends StatelessWidget {
                     title: const Text("FARDALINTRANS"),
                 ),
                 body: Container(
-                    color: const Color(0xe9ecefff),
+                    color: const Color(0xffffffff),
                     child: ListView(
                         shrinkWrap: true,
                         children: [
-                          // Work In Progress
+                            Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                    Container(
+                                        alignment: Alignment.center,
+                                        margin: const EdgeInsets.fromLTRB(0, 150, 0, 20),
+                                        child: Image.asset("assets/img/ThankYou.png", fit: BoxFit.cover, width: double.infinity,),
+                                    ),
+                                    Container(
+                                        alignment: Alignment.center,
+                                        margin: const EdgeInsets.fromLTRB(25, 0, 25, 200),
+                                        child: const Text('Terima Kasih pembayaran anda sudah diterima',
+                                            style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 26,
+                                                fontFamily: 'Poppins',
+                                                fontWeight: FontWeight.bold
+                                            ),
+                                            textAlign: TextAlign.center,
+                                        )
+                                    )
+                                ],
+                            )
                         ]
                     )
                 )
